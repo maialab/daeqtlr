567f3086bca3b3badfcdf6a3873b6e5b77058fba
Ramiro Magno <ramiro.magno@gmail.com>
2022-04-30 23:09:02 +0100

    Add function to easily retrieve path of extdata data sets: `daeqtlr_example()`

---
+++ R/daeqtlr_example.R
@@ -0,0 +1,17 @@
+#' Get path to daeqtlr example
+#'
+#' daeqtlr comes bundled with a number of sample files in its `inst/extdata`
+#' directory. This function make them easy to access.
+#'
+#' @param file Name of file. If `NULL`, the example files will be listed.
+#' @export
+#' @examples
+#' daeqtlr_example()
+#' daeqtlr_example("zygosity.csv")
+daeqtlr_example <- function(file = NULL) {
+  if (is.null(file)) {
+    dir(system.file("extdata", package = "daeqtlr"))
+  } else {
+    system.file("extdata", file, package = "daeqtlr", mustWork = TRUE)
+  }
+}
